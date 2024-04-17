25bd59d00214ad3c280a6f05628ab19ff7404cbe
Golam Azam <bcdazam@gmail.com>
2024-04-17 18:19:15 +0600

    module overview use case

---
+++ 14-1.js
@@ -0,0 +1,5 @@
+//14-1 Module Introduction Apply JavaScript 
+
+
+//Discussed about previous lessons like, loop, function, array etc.
+//Where and when the function, variable, loop etc will be apply
