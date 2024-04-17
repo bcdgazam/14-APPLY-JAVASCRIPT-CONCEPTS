607fe2f7ddcc447ae212121066980c43ffc26dfa
Golam Azam <bcdazam@gmail.com>
2024-04-17 22:04:54 +0600

    let and const

--- 14-3.js
+++ 14-3.js
@@ -1,1 +1,10 @@
 //14-3 variable let and const and how to use them
+
+//let example
+let age = 29;
+age = 50;
+console.log(age);
+
+//const example
+const contName = 'Bangladesh';
+console.log(contName);
