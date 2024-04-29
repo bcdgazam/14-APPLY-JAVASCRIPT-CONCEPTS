9444907badf46ee33c623f96e1cbf09062acc011
Golam Azam <bcdazam@gmail.com>
2024-04-29 22:52:20 +0600

    inch to feet

--- 14-12.js
+++ 14-12.js
@@ -1,3 +1,16 @@
 //14-12 Apply Basic JavaScript Module Summary
 
 //Discussed about previous lessons.
+
+/*
+    14-2 Unit Convert Inch to Feet use variable and function - Running
+    14-3 variable let and const and how to use them
+    14-4 check whether a year is a Leap Year or not
+    14-5 Calculate Factorial of a number using for loop
+    14-6 Calculate Factorial of a number using a while loop
+    14-7 Calculate Factorial in a Recursive function
+    14-8 Create a Fibonacci Series using a for loop
+    14-9 Fibonacci Element in a Recursive Way
+    14-10 Create Fibonacci series in a recursive way
+    14-11 Check whether a number is a Prime Number or not
+*/
--- practice.js
+++ practice.js
@@ -1,1 +1,5 @@
-//practice here
+let inch = 200;
+let result = inch/12;
+let result2 = result.toFixed(0);
+result2 = Math.abs(result2);
+console.log(typeof(result2));
