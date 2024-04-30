e3492e9c922e2a8e5eb5567f55a16dfcaaf2cf36
Golam Azam <bcdazam@gmail.com>
2024-04-30 06:53:16 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,12 +1,15 @@
-// let inch = 300;
-// let feet = inch/12;
-// console.log(feet);
-
-
 function inchToFeet(inch){
-    let feet = inch/12;
+    let feet = inch/4;
     return feet;
 }
 
 let result1 = inchToFeet(300);
 console.log(result1);
+let result2 = inchToFeet(600);
+console.log(result2);
+
+let num1 = 50;
+num1 = 90;
+console.log(num1);
+const name2 ='Bangladesh';
+console.log(name2);
