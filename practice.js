e75ef44c5f7152f259319526a9661c0bc7594133
Golam Azam <bcdazam@gmail.com>
2024-04-30 07:55:47 +0600

    leap year check

--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+/*
 function inchToFeet(inch){
     let feet = inch/4;
     return feet;
@@ -12,4 +13,56 @@ let num1 = 50;
 num1 = 90;
 console.log(num1);
 const name2 ='Bangladesh';
-console.log(name2);
+console.log(name2); */
+
+//check by divider
+
+// let leapYear = 1090;
+// let reminderOne = leapYear / 4;
+// console.log(reminderOne);
+
+// function checkLeapYear(leapYear){
+//     let reminderTwo = leapYear % 4;
+//     if ( reminderTwo == 0 ){
+//         console.log('This year is a leap year!');
+//     }
+//     else {
+//         console.log('Sorry, This is not a leap Year.')
+//     }
+// }
+
+// checkLeapYear(1980);
+// checkLeapYear(2058);
+
+// function checkLeapYear(year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//         console.log(year + ' is a leap year');
+//     } else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
+
+// checkLeapYear(1980);
+
+
+// function checkLeapYear (year){
+//     if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+//         console.log(year + ' is a leap year');
+//     }
+//     else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
+
+// checkLeapYear(1080);
+
+function checkLeapYer(year){
+    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+        console.log(year + ' is a leap year!')
+    }
+    else{
+        console.log( year + ' is not a leap year')
+    }
+}
+
+checkLeapYer(1080);
