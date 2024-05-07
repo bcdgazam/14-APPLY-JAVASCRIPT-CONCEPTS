8267f6e97a7d7698776aa95db0edd477e8ac057d
Golam Azam <bcdazam@gmail.com>
2024-05-07 10:35:03 +0600

    inch to feet by function

--- practice.js
+++ practice.js
@@ -1,3 +1,13 @@
-let inchToFeet = 200;
-let feet = inchToFeet/12;
-console.log(feet)
+// let inchToFeet = 200;
+// let feet = inchToFeet/12;
+// console.log(feet)
+
+//inch to feet by function
+
+function inchToFeet(n){
+    let feet = n/12;
+    return feet;
+}
+
+let result = inchToFeet(350);
+console.log(result);
