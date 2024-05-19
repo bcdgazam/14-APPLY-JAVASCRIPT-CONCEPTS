85094e43bd84e378f919bf96efa643efa4a21b68
Golam Azam <bcdazam@gmail.com>
2024-05-19 18:21:05 +0600

    14 done

--- practice.js
+++ practice.js
@@ -1,14 +1,44 @@
-//inch to feet
+//inch to feet using variable
 
 // let inch = 24;
-
 // let feet = inch / 12;
 // console.log(feet);
 
-function inchToFeet(inch){
-    let feet = inch/12;
-    return feet;
-}
+//inch to feet using function.
 
-let result = inchToFeet(24);
-console.log(result);
+// function inchToFeet(n){
+//     let feet = n / 12;
+//     return feet;
+// }
+
+// let result = inchToFeet(24)
+// console.log(result);
+
+// let leapyear1 = 1230;
+// let reminder1 = leapyear1 / 4;
+// console.log(reminder1);
+
+// let leapyear1 = 2025;
+// let reminder1 = leapyear1 % 4;
+// console.log(reminder1 == 0);
+
+//check leap year using function.
+
+// function leapYearCheck(year){
+//     if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 )
+// }
+
+//factorial
+
+// let factorial = 1;
+
+// for(let i = 1; i <= 6; i++){
+//     factorial = factorial * i;
+//     console.log(i, factorial);
+// }
+
+// let fact = 1;
+// for(let i = 1; i < 10; i ++){
+//     fact = fact * i;
+//     console.log(fact);
+// }
